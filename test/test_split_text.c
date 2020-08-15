647dd339c3745d6a5e2ce72c4b27cfea37f3b5ed
stepin104923 <69148040+stepin104923@users.noreply.github.com>
2020-08-15 19:48:56 +0530

    Update Test Cases
    Make all input strings allocated in the heap
    Enable other test cases

--- test/test_split_text.c
+++ test/test_split_text.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "unity.h"
 #include "split_text.h"
 
@@ -25,12 +26,13 @@ void compare_pcs(char** expected, char** actual)
 /* Test for general text splitting*/
 void test_split_text(void)
 {
-	char *test_str = "my_name_is_edcast_future_skills";
+	char *test_str = strdup("my_name_is_edcast_future_skills");
 	char *test_pcs[] = { "my", "name", "is", "edcast", "future", "skills", NULL};
 
-	char **pcs = split_text(test_str);
+	char **pcs;
 
 	// Test Split
+	pcs = split_text(test_str);
 	compare_pcs(test_pcs, pcs);
 
 	free_pieces(pcs);
@@ -39,64 +41,81 @@ void test_split_text(void)
 /*  Test for non-mutablility of input  */
 void test_mutability(void)
 {
-	char *test_str = "my_name_is_edcast_future_skills";
+	char *test_str = strdup("my_name_is_edcast_future_skills");
 	char *test_str_copy = "my_name_is_edcast_future_skills";
 
 	char **pcs;
 
 	// Test Mutability
-	// TODO : Compare test_str & test_str_copy
+	pcs = split_text(test_str);
+	TEST_ASSERT_EQUAL_STRING(test_str, test_str_copy);
+
+	free_pieces(pcs);
 }
 
 /* Test for handling successive delimitters */
 void test_double_delim(void)
 {
-	char *test_str = "my_name_is__edcast_future___skills";
+	char *test_str = strdup("my_name_is__edcast_future___skills");
 	char *test_pcs[] = { "my", "name", "is", "edcast", "future", "skills", NULL};
 
 	char **pcs;
 
 	// Test Consistency for multiple delimiters
-	// TODO : Compare pcs & test_pcs
+	pcs = split_text(test_str);
+	compare_pcs(test_pcs, pcs);
+
+	free_pieces(pcs);
 }
 
 void test_delim_at_ends(void)
 {
-	char *test_str_a = "_my_name_is_edcast_future_skills";
-	char *test_str_b = "my_name_is_edcast_future_skills_";
+	char *test_str_a = strdup("_my_name_is_edcast_future_skills");
+	char *test_str_b = strdup("my_name_is_edcast_future_skills_");
 	char *test_pcs[] = { "my", "name", "is", "edcast", "future", "skills", NULL};
 
 	char **pcs_a, **pcs_b;
 
 	// Test Split A
-	// TODO : Compare pcs_a & test_pcs
+	pcs_a = split_text(test_str_a);
+	compare_pcs(test_pcs, pcs_a);
 	
 	// Test Split B
-	// TODO : Compare pcs_b & test_pcs
+	pcs_b = split_text(test_str_b);
+	compare_pcs(test_pcs, pcs_b);
+
+	free_pieces(pcs_a);
+	free_pieces(pcs_b);
 }
 
 /* Test for handling string consisting only of delimitters */
 void test_delim_only(void)
 {
-	char *test_str = "__";
+	char *test_str = strdup("__");
 	char *test_pcs[] = { NULL};
 
 	char **pcs;
 
 	// Test Consistency for string with delimiters only
-	// TODO : Compare pcs & test_pcs
+	pcs = split_text(test_str);
+	compare_pcs(test_pcs, pcs);
+
+	free_pieces(pcs);
 }
 
 /* Test for handling abscence of delimitters */
 void test_none(void)
 {
-	char *test_str = "my name is edcast future skills";
+	char *test_str = strdup("my name is edcast future skills");
 	char *test_pcs[] = { "my name is edcast future skills", NULL};
 
 	char **pcs;
 
 	// Test Consistency for string with delimiters only
-	// TODO : Compare pcs & test_pcs
+	pcs = split_text(test_str);
+	compare_pcs(test_pcs, pcs);
+
+	free_pieces(pcs);
 }
 
 int test_main(void)
